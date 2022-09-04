07437ea726b6a65fb0e61f2f59d6e683faacda91
Romi Julianto <romyjulians@gmail.com>
2022-09-04 19:26:31 +0700

    update: config.php and database.php for hosting 000webhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/furniture/';
+$config['base_url'] = 'https://furniture-bgrlogistik.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
